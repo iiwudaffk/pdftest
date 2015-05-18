e0d6ff55f9328864201b2809509b29462fd64dc6
aekasitcpe <aekasitcpe@gmail.com>
2015-05-19 03:43:03 +0700

    fix server

--- helloworld/server.js
+++ helloworld/server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(1337, '128.199.229.29');
+console.log('Server running at http://128.199.229.29/:1337/');
