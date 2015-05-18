f3cd00a5abf87beaed231493105bdb22430d3b18
aekasitcpe <aekasitcpe@gmail.com>
2015-05-19 03:49:47 +0700

    show log ip

--- helloworld/server.js
+++ helloworld/server.js
@@ -2,5 +2,30 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, '128.199.229.29');
+}).listen(1337,'128.199.229.29');
 console.log('Server running at http://128.199.229.29/:1337/');
+
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0
+    ;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+  });
+});
